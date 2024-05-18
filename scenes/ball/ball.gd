8b627317750facc032d95c6f5e947a565674365f
Bruno Mello <bruno.barros.mello@gmail.com>
2024-05-18 13:45:56 -0300

    sounds + keyboard events

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -1,13 +1,18 @@
 extends Area2D
 
-var speed: int = 500
+var initial_speed: int = 500
+var speed: int = initial_speed
 var initial_pos: Vector2 = Vector2(640, 360)
 var dir: Vector2 = Vector2(0,0)
 
 var max_y = 720
 var min_y = 0
+var min_x = 0
+var max_x = 1280
 
 @onready var timer: Timer = $Timer
+@onready var player_hit_sound = $PlayerHitSound
+@onready var wall_hit_sound = $WallHitSound
 
 func _ready():
 	wait_timer()
@@ -15,7 +20,7 @@ func _ready():
 
 func _process(delta):
 	move(delta)
-	colide()
+	colide_wall()
 
 
 func pick_direction() -> void:
@@ -34,15 +39,18 @@ func wait_timer() -> void:
 func reset() -> void:
 	position = initial_pos
 	pick_direction()
+	speed = initial_speed
 
-func colide() -> void:
+func colide_wall() -> void:
 	if position.y <= min_y or position.y >= max_y:
 		dir.y *= -1
+		if position.x >= min_x and position.x <= max_x:
+			wall_hit_sound.play()
 
 func _on_body_entered(body):
 	if body.is_in_group("players"):
 		dir.x *= -1
-
+		player_hit_sound.play()
 
 func _on_timer_timeout():
 	reset()
--- scenes/game_manager/game_manager.gd
+++ scenes/game_manager/game_manager.gd
@@ -7,12 +7,18 @@ var player2_score: int = 0
 @onready var p1_score_label = $"../UI/ScorePanel/P1Score"
 @onready var p2_score_label = $"../UI/ScorePanel/P2Score"
 
-func _ready():
-	pass
+@onready var goal_sound = $GoalSound
 
 
-func _process(delta):
-	pass
+func _process(delta) -> void:
+	handle_inputs()
+
+func handle_inputs() -> void:
+	if Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+	elif Input.is_action_just_pressed("exit"):
+		get_tree().quit()
+	
 
 func update_scores() -> void:
 	p1_score_label.set_text(str(player1_score))
@@ -20,10 +26,18 @@ func update_scores() -> void:
 	
 func _on_goal_1_area_entered(area) -> void:
 	player2_score += 1
+	goal_sound.play()
 	update_scores()
 	ball.wait_timer()
 
 func _on_goal_2_area_entered(area):
 	player1_score += 1
+	goal_sound.play()
+	update_scores()
+	ball.wait_timer()
+
+func reset() -> void:
+	player1_score = 0
+	player2_score = 0
 	update_scores()
 	ball.wait_timer()
